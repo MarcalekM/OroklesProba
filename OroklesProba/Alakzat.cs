c204da3af4efd6be1ba3825afdb65fc2ee0b9cac
Máté Marcalek <matemarcalek@gmail.com>
2023-10-09 11:07:09 +0200

    Feltöltés

---
+++ OroklesProba/Alakzat.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OroklesProba
+{
+    public abstract class Alakzat
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public abstract void Rajzol();
+
+        public Alakzat(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class Teglalap : Alakzat
+    {
+        public int Szelesseg { get; private set; }
+        public int Magassag { get; private set; }
+
+        public Teglalap(int x, int y, int szelesseg, int magassag) : base(x, y)
+        {
+            Szelesseg = szelesseg;
+            Magassag = magassag;
+        }
+
+        public override void Rajzol()
+        {
+            Console.SetCursorPosition(X, Y);
+            for (int i = 0; i < Magassag; i++)
+            {
+                for (int j = 0; j < Szelesseg; j++)
+                {
+                    if (i == 0 || i == Magassag - 1) Console.Write("+");
+                    else
+                    {
+                        if (j == 0 || j == Szelesseg - 1) Console.Write("+");
+                        else Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    public sealed class Negyzet : Teglalap
+    {
+        public Negyzet(int x, int y, int meret) : base(x, y, meret, meret) { }
+    }
+}
---
+++ OroklesProba/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OroklesProba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Teglalap(0, 0, 20, 12);
+            var n = new Negyzet(0, 15, 10);
+            t.Rajzol();
+            n.Rajzol();
+            Console.ReadLine();
+        }
+    }
+}
